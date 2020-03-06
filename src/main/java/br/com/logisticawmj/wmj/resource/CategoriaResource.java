66b6ad994cc3e18ca7e5555a8219a45cc60df4ee
WellingtonMJose <wellingtonmoacirjose@gmail.com>
2020-03-06 18:20:46 -0300

    Testando o rest

---
+++ src/main/java/br/com/logisticawmj/wmj/resource/CategoriaResource.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.logisticawmj.wmj.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author wellington
+ */
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+    
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String listar() {
+        return "Rest está funcionando!!!";
+    }
+
+}
