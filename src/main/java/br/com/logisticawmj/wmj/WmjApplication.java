c9edafc29ff76ae2bd318ebbb92843e8d4f0821b
WellingtonMJose <wellingtonmoacirjose@gmail.com>
2020-03-06 17:46:17 -0300

    Criação

---
+++ src/main/java/br/com/logisticawmj/wmj/WmjApplication.java
@@ -0,0 +1,13 @@
+package br.com.logisticawmj.wmj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/logisticawmj/wmj/WmjApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.logisticawmj.wmj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
