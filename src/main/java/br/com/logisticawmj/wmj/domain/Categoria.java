e611b8f5deb8ea0121065dabe90cfa7d8b5e1018
WellingtonMJose <wellingtonmoacirjose@gmail.com>
2020-03-12 15:46:48 -0300

    criado o banco de dados no h2

--- src/main/java/br/com/logisticawmj/wmj/domain/Categoria.java
+++ src/main/java/br/com/logisticawmj/wmj/domain/Categoria.java
@@ -7,15 +7,23 @@ package br.com.logisticawmj.wmj.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
  * @author wellington
  */
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
+    
     private String name;
 
     public Categoria() {
